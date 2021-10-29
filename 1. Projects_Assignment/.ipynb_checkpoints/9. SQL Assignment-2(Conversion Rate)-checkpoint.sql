ef1b387a3e8fe122654491393942b513eb41b3c8
StephenKinggg <87016594+StephenKinggg@users.noreply.github.com>
2021-10-29 18:47:28 +0300

    some files for sql project added.

---
+++ 1. Projects_Assignment/.ipynb_checkpoints/9. SQL Assignment-2(Conversion Rate)-checkpoint.sql
@@ -0,0 +1,49 @@
+DROP TABLE IF EXISTS conver_rate
+CREATE TABLE conver_rate
+	(Visitor_ID INT NOT NULL,
+	Adv_ID VARCHAR(50),
+	Actionn VARCHAR(50)
+	)
+
+INSERT INTO conver_rate(Visitor_ID, Adv_ID, Actionn )
+VALUES  (1, 'A', 'Left'),
+		(2, 'A', 'Order'),
+		(3, 'B', 'Left'),
+		(4, 'A', 'Order'),
+		(5, 'A', 'Review'),
+		(6, 'A', 'Left'),
+		(7, 'B', 'Left'),
+		(8, 'B', 'Order'),
+		(9, 'B', 'Review'),
+		(10, 'A','Review')
+		
+
+SELECT *
+FROM conver_rate
+
+SELECT Adv_ID, COUNT(Actionn) Total_Action
+FROM conver_rate
+GROUP BY Adv_ID
+
+
+SELECT Adv_ID,subtotal
+FROM (SELECT Adv_ID, actionn, COUNT(Actionn) subtotal
+FROM conver_rate
+GROUP BY Adv_ID, actionn) T1
+WHERE T1.actionn='Order'
+
+
+SELECT C1.Adv_ID Adv_Type, ((T2.subtotal*1.0) / (C1.Total_Action)*1.0) Conversion_Rate
+FROM (SELECT Adv_ID, COUNT(Actionn) Total_Action
+FROM conver_rate
+GROUP BY Adv_ID) C1 ,
+(SELECT Adv_ID,subtotal
+FROM (SELECT Adv_ID, actionn, COUNT (Actionn) subtotal
+FROM conver_rate
+GROUP BY Adv_ID, actionn) T1
+WHERE T1.actionn='Order') T2
+WHERE C1.Adv_ID=T2.Adv_ID
+
+
+
+
