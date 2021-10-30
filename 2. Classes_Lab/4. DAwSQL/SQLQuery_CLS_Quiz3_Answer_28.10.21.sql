b22b4765b8e6dca13eebc3b2cd52dfd1d8440ccd
StephenKinggg <87016594+StephenKinggg@users.noreply.github.com>
2021-10-31 00:04:36 +0300

    sql file added.

--- 2. Classes_Lab/4. DAwSQL/SQLQuery_CLS_Quiz3_Answer_28.10.21.sql
+++ 2. Classes_Lab/4. DAwSQL/SQLQuery_CLS_Quiz3_Answer_28.10.21.sql
@@ -1,27 +1,9 @@
 --Mini Quiz:
 --1.Answer:
-SELECT tracks.TrackId,tracks.Name, albums.AlbumId
-FROM tracks,albums
-WHERE tracks.AlbumId=albums.AlbumId
-AND albums.Title='Outbreak'
+SELECT first_name, 
+	last_name, 
+	hire_date, 
+	term_date, 
+	JULIANDAY(term_date) - JULIANDAY(hire_date) AS date_diff
 
---2.Answer:
-SELECT CustomerId,customers.FirstName , customers.LastName
-FROM customers, employees
-WHERE customers.SupportRepId = employees.EmployeeId
-AND employees.Country='Canada'
-ORDER BY CustomerId
-
---3.Answer:
-
-SELECT DISTINCT tracks.AlbumId, albums.Title, T1.albumbytes
-FROM tracks,albums, (SELECT tracks.AlbumId, albums.Title, SUM(tracks.bytes) albumbytes
-FROM tracks, albums
-WHERE tracks.AlbumId=albums.AlbumId
-GROUP BY tracks.AlbumId) T1
-WHERE tracks.AlbumId=T1.AlbumId
-AND albums.AlbumId=T1.AlbumId
-AND T1.albumbytes < 5000000
-
-
-WHERE SUM(tracks.bytes) < 5000000
+FROM employees;
