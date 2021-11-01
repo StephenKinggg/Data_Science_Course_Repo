0761d5ec74e4f466768375337f4ffaa00860528c
StephenKinggg <87016594+StephenKinggg@users.noreply.github.com>
2021-11-01 20:01:00 +0300

    some files added.

--- 1. Projects_Assignment/DAwSQL_Project_E-Commerce Data and Customer Retention Analysis/DAwSQL_Project_E-Commerce Data and Customer Retention Analysis_File_30.10.21.sql
+++ 1. Projects_Assignment/DAwSQL_Project_E-Commerce Data and Customer Retention Analysis/DAwSQL_Project_E-Commerce Data and Customer Retention Analysis_File_30.10.21.sql
@@ -4,22 +4,44 @@
 
 --E-Commerce Project Solution
 
-
-
 --1. Join all the tables and create a new table called combined_table. (market_fact, cust_dimen, orders_dimen, prod_dimen, shipping_dimen)
 
-CREATE VIEW combined_table AS
-(SELECT A.Ord_id,A.Order_Date,A.Order_Priority,
-		B.Cust_id,B.Customer_Name,B.Customer_Segment,B.Province,B.Region,
-		C.Order_ID,C.Ship_id,C.Ship_Date,C.Ship_Mode,
-		D.Prod_id,D.Product_Category,D.Product_Sub_Category,
-		E.Discount,E.Order_Quantity,E.Product_Base_Margin,E.Sales
-FROM orders_dimen A, cust_dimen B, shipping_dimen C, prod_dimen D, market_fact E
-WHERE A.Ord_id=E.Ord_id
-AND E.Cust_id=B.Cust_id
-AND E.Ship_id=C.Ship_id
-AND E.Prod_id=D.Prod_id)
+WITH T1 AS
+(
+SELECT A.Ord_id,B.Cust_id,C.Ship_id,D.Prod_id, 
+	   A.Order_Date,A.Order_Priority,
+	   B.Customer_Name,B.Customer_Segment,B.Province,B.Region,
+	   C.Ship_Date,C.Ship_Mode,
+	   D.Product_Category,D.Product_Sub_Category,
+	   E.Discount,E.Order_Quantity,E.Product_Base_Margin,E.Sales
+FROM market_fact E 
+JOIN orders_dimen A ON A.Ord_id=E.Ord_id
+JOIN cust_dimen B ON E.Cust_id=B.Cust_id
+JOIN shipping_dimen C ON E.Ship_id=C.Ship_id
+JOIN prod_dimen D ON E.Prod_id=D.Prod_id
+)
+CREATE TABLE combined_table AS
+SELECT * FROM T1
 
+
+DROP TABLE IF EXISTS combined_table
+CREATE TABLE combined_table
+  AS  
+(SELECT E.Ord_id,E.Cust_id,E.Ship_id,E.Prod_id, 
+	   A.Order_Date,A.Order_Priority,
+	   B.Customer_Name,B.Customer_Segment,B.Province,B.Region,
+	   C.Ship_Date,C.Ship_Mode,
+	   D.Product_Category,D.Product_Sub_Category,
+	   E.Discount,E.Order_Quantity,E.Product_Base_Margin,E.Sales
+FROM market_fact E 
+JOIN orders_dimen A ON A.Ord_id=E.Ord_id
+JOIN cust_dimen B ON B.Cust_id=E.Cust_id
+JOIN shipping_dimen C ON C.Ship_id=E.Ship_id
+JOIN prod_dimen D ON D.Prod_id=E.Prod_id);
+
+
+SELECT *
+FROM combined_table
 
 --///////////////////////
 
@@ -31,12 +53,12 @@ FROM market_fact
 
 WITH T1 AS
 (
-SELECT TOP 3 Cust_id, SUM(Order_Quantity) Total_Order_Q
+SELECT TOP 3 Cust_id, count(Order_Quantity) count_of_order
 FROM market_fact
 GROUP BY Cust_id
-ORDER BY Total_Order_Q DESC
+ORDER BY count_of_order DESC
 )
-SELECT A.Cust_id, A.Customer_Name
+SELECT A.Cust_id, A.Customer_Name, T1.count_of_order
 FROM cust_dimen A, T1
 WHERE T1.Cust_id=A.Cust_id
 
@@ -44,18 +66,34 @@ WHERE T1.Cust_id=A.Cust_id
 --/////////////////////////////////
 
 
-
 --3.Create a new column at combined_table as DaysTakenForDelivery that contains the date difference of Order_Date and Ship_Date.
 --Use "ALTER TABLE", "UPDATE" etc.
 
+ALTER TABLE combined_table
+ADD DaysTakenForDelivery INT;
 
+UPDATE combined_table
+SET DaysTakenForDelivery=DATEDIFF(Day, Order_Date, Ship_Date)
 
+SELECT TOP 100 *
+FROM combined_table
 
 --////////////////////////////////////
 
 
 --4. Find the customer whose order took the maximum time to get delivered.
 --Use "MAX" or "TOP"
+
+WITH T1 AS
+(
+SELECT A.Ord_id, A.Ship_id, B.Order_Date, C.Ship_Date, A.Cust_id 
+FROM market_fact A, orders_dimen B, shipping_dimen C
+WHERE A.Ord_id=B.Ord_id
+AND A.Ship_id=C.Ship_id
+)
+SELECT *, DATEDIFF(Day,Order_Date, Ship_Date) DaysTakenForDelivery  
+FROM T1
+ORDER BY DaysTakenForDelivery DESC
 
 
 
