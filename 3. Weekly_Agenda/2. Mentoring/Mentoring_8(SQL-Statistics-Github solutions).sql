e1e08efaacfef83ad52663f07bd665d892b14080
StephenKinggg <87016594+StephenKinggg@users.noreply.github.com>
2021-11-08 22:02:15 +0300

    some files updated and added.

---
+++ 3. Weekly_Agenda/2. Mentoring/Mentoring_8(SQL-Statistics-Github solutions).sql
@@ -0,0 +1,43 @@
+--/*
+
+-- Question_1:
+
+Answer the following questions according to bikestore database :runner::skin-tone-3::runner::skin-tone-3::runner::skin-tone-3:@C9340-Yasir :runner::skin-tone-3::runner::skin-tone-3::runner::skin-tone-3::runner::skin-tone-3:
+- What is the sales quantity of product according to the brands and sort them highest-lowest
+- Select the top 5 most expensive products
+- What are the categories that each brand has
+- Select the avg prices according to brands and categories
+- Select the annual amount of product produced according to brands
+- Select the least 3 products in stock according to stores.
+- Select the store which has the most sales quantity in 2016
+- Select the store which has the most sales amount in 2016
+- Select the personnel which has the most sales amount in 2016
+- Select the least 3 sold products in 2016 and 2017 according to city.
+
+-- Question_2:
+
+1. Find the customers who placed at least two orders per year. @C8134-Balkan Dogu
+2. Find the total amount of each order which are placed in 2018. Then categorize them according to limits stated below.(You can use case when statements here)
+If the total amount of order   
+  less then 500 then “very low”
+  between 500 - 1000 then “low”
+  between 1000 - 5000 then “medium”
+  between 5000 - 10000 then “high”
+  more then 10000 then “very high” 
+3. By using Exists Statement find all customers who have placed more than two orders.
+4. Show all the products and their list price, that were sold with more than two units in a sales order.
+5. Show the total count of orders per product for all times. (Every product will be shown in one line and the total order count will be shown besides it)
+6. Find the products whose list prices are more than the average list price of products of all brands
+
+--Question-3:
+
+7. In how many ways, a group of 3 boys and 2 girls can be formed out of a total of 5 boys and 5 girls?
+8.A woman has 6 blouses, 4 skirts, and 5 pairs of shoes. How many different outfits consisting of a blouse, a skirt, and a pair of shoes can she wear?
+9. Serena Williams won the 2010 Wimbledon Ladies’ Singles Championship. For the seven matches she played in the tournament, her total number of first serves was 379, 
+total number of good first serves was 256, and total number of double faults was 15.
+a. Find the probability that her first serve is good.
+b. Find the conditional probability of double faulting, given that her first serve resulted in a fault.
+c. On what percentage of her service points does she double fault?
+10. Suppose that in the world exist a very rare disease. The chance for anyone to have this disease is 0.1%. You want to know whether you are infected so you go 
+take a test, and the test results come positive. The accuracy of the test is 99%, meaning that 99% of the people who have the disease will test positive,
+and 99% of the people who do not have the disease will test negative. What is the chance that you are actually infected? .......Bayes Theory.....
