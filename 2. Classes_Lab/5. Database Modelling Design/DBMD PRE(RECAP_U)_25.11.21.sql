332d8c4fff5ba3d0975001f16f007f09676feecb
StephenKinggg <87016594+StephenKinggg@users.noreply.github.com>
2021-11-26 00:29:47 +0300

    some files updated.

--- 2. Classes_Lab/5. Database Modelling Design/DBMD PRE(RECAP_U)_25.11.21.sql
+++ 2. Classes_Lab/5. Database Modelling Design/DBMD PRE(RECAP_U)_25.11.21.sql
@@ -187,3 +187,212 @@ SELECT *, FirstName+ ' '+ MiddleName+ ' ' + LastName AS Name,
 		CAST((AccountKey AS VARCHAR) + AccountDescription) AS 'KEY' --BURADA CAST ÝLE AccountKey veri tipini VARCHAR çevirdik.
 FROM [Person].[Person]
 
+/*
+	(),
+	=,
+	<, >,
+	<= ve >=,
+	<>, !=
+
+	AND: Her ikisininde doðru olmasý gerekir. Mesela maaþ<2000 AND department='IT'
+
+	OR : Ýkisinden herhangi birinin doðru olmasý yeterli.  Mesela maaþ<2000 OR department='IT'
+
+	NOT : NOT firstname ='John' adý john olmayanlarý getirir.
+	*/
+
+SELECT *
+FROM [HumanResources].[Employee]
+WHERE BirthDate>'1974-01-01' AND HireDate < '2008-01-01'
+
+SELECT *
+FROM [HumanResources].[Employee]
+WHERE BirthDate>'1974-01-01' OR HireDate < '2008-01-01'
+
+SELECT *
+FROM [HumanResources].[Employee]
+WHERE 
+(BirthDate>'1974-01-01' AND HireDate < '2008-01-01')
+AND OrganizationLevel=3
+
+SELECT *
+FROM [HumanResources].[Employee]
+WHERE 
+(BirthDate>'1974-01-01' AND HireDate < '2008-01-01')
+OR OrganizationLevel=3
+
+SELECT *
+FROM [HumanResources].[Employee]
+WHERE 
+(BirthDate>'1974-01-01' AND HireDate < '2008-01-01')
+AND OrganizationLevel!=3  --AND NOT kullanabiliriz ancak OrganizationLevel=3 yazýlmalý.
+
+SELECT *
+FROM [HumanResources].[Employee]
+WHERE 
+(BirthDate>'1974-01-01' AND HireDate < '2008-01-01')
+AND JobTitle IN('Marketing Assistant')
+
+SELECT *
+FROM [HumanResources].[Employee]
+WHERE 
+(BirthDate>'1974-01-01' AND HireDate < '2008-01-01')
+AND JobTitle NOT IN('Marketing Assistant')
+
+SELECT *
+FROM [HumanResources].[Employee]
+WHERE 
+(BirthDate>'1974-01-01' AND HireDate < '2008-01-01')
+AND BusinessEntityID BETWEEN 3 AND 16 
+
+SELECT *
+FROM [HumanResources].[Employee]
+WHERE JobTitle LIKE 'M%'     --'%M', '%M%'
+
+SELECT *
+FROM [HumanResources].[Employee]
+WHERE JobTitle LIKE '%S_'   --SONU S VE YANINA BÝR KARAKTER ALARAK OLANLARI ÇAÐIRIR.
+
+SELECT *
+FROM [HumanResources].[Employee]
+WHERE JobTitle LIKE '[A-C]%'   --ÝLK karakteri A-C arasýnda baþlayanlarý getirir.
+
+SELECT *
+FROM [HumanResources].[Employee]
+WHERE BusinessEntityID LIKE '[1-2]%' --ÝLK karakteri 1 ve 2 ile baþlayanlarý getirir.
+
+SELECT *
+FROM [HumanResources].[Employee]
+WHERE OrganizationLevel IS NULL  
+
+SELECT *
+FROM [HumanResources].[Employee]
+WHERE OrganizationLevel IS NOT NULL 
+
+
+SELECT SalesOrderID, ProductID, SUM(OrderQty) SumQuanty
+FROM [Sales].[SalesOrderDetail]
+WHERE SalesOrderID>5000 AND ProductID>750 --BURADAKÝ WHERE GROUP BY YAPILMADAN ÖNCEKÝ TABLE DA ÝÞLEM YAPAR VE GROUP BY BUNUN ÜZERÝNE YAPILIR.
+GROUP BY SalesOrderID, ProductID
+HAVING SalesOrderID>50000  --HAVING ÝSE GROUP BY DAN SONRAKÝ TABLE ÜZERÝNDE ÝÞLEM YAPAR.
+ORDER BY SalesOrderID
+
+-------DATABASE-----
+
+
+USE UniversityCourseDB
+
+--DROP TABLE Course
+CREATE TABLE Course(
+CourseID NVARCHAR(10) PRIMARY KEY,
+CourseTitle NVARCHAR(50),
+TutorID NVARCHAR(10),
+ClassID NVARCHAR(10)
+)
+
+CREATE TABLE Student(
+StudentID INT PRIMARY KEY,
+StudentName NVARCHAR(50),
+StudentAdress NVARCHAR(50),
+StudentBirtDate DATE,
+ClassID NVARCHAR(10),
+EnrollDate DATE,
+GraduateDate DATE
+)
+
+--DROP TABLE Class
+CREATE TABLE Class(
+ClassID NVARCHAR(10) PRIMARY KEY,
+ClassName NVARCHAR(50)
+)
+
+--DROP TABLE Tutor
+CREATE TABLE Tutor(
+TutorID NVARCHAR(10) PRIMARY KEY,
+TutorName NVARCHAR(50),
+TutorTitle NVARCHAR(15),
+ClassID NVARCHAR(10)
+)
+--DROP TABLE Enrollment
+CREATE TABLE Enrollment(
+EnrollID INT PRIMARY KEY,
+StudentID INT,
+CourseID NVARCHAR(10),
+SessionID INT
+)
+
+--DROP TABLE Success
+CREATE TABLE Success(
+SuccessID INT PRIMARY KEY,
+CourseID NVARCHAR(10),
+StudentID INT,
+MidtermExam INT,
+FinalExam INT,
+MakeupExam INT
+)
+
+INSERT INTO [dbo].[Course] VALUES('EM220', 'Data Science','DS001', '02')
+INSERT INTO [dbo].[Course] VALUES('EM250', 'Statistics','DS002', '03')
+INSERT INTO [dbo].[Course] VALUES('EK300', 'Tableau','DS003', '04')
+INSERT INTO [dbo].[Course] VALUES('EK310', 'SQL','DS004', '05')
+INSERT INTO [dbo].[Course] VALUES('EK350', 'DBMD','DS005', '06')
+
+INSERT INTO [dbo].[Student] VALUES(1212, 'Carry Spielberg', 'Washington','1994-12-15', '02', '2010-01-01', NULL)
+INSERT INTO [dbo].[Student] VALUES(1214, 'Carl Hanz', 'Utah','1992-12-15', '02', '2010-05-01', NULL)
+INSERT INTO [dbo].[Student] VALUES(1216, 'Mary Hely', 'California','1996-12-15', '04', '2010-03-01', NULL)
+INSERT INTO [dbo].[Student] VALUES(1218, 'Catherine Spiel', 'Washington','1995-12-15', '06', '2010-03-01', NULL)
+
+INSERT INTO [dbo].[Class] VALUES('02', 'Machine Engineering')
+INSERT INTO [dbo].[Class] VALUES('04', 'Electronic Engineering')
+INSERT INTO [dbo].[Class] VALUES('06', 'Robotic Engineering')
+
+INSERT INTO [dbo].[Tutor] VALUES('EM220', 'Prof','Alex Hunter', '02')
+INSERT INTO [dbo].[Tutor] VALUES('EM250', 'Prof','George Michael', '03')
+INSERT INTO [dbo].[Tutor] VALUES('EK300', 'Prof','Karl Hansek', '04')
+INSERT INTO [dbo].[Tutor] VALUES('EK310', 'Prof','Mary Island', '05')
+
+
+ALTER TABLE [dbo].[Tutor]
+ADD CONSTRAINT FKClassTutor        --Burayý rastgele veriyoruz. Hangi tablolar arasýnda olacaksa ona yakýn bir isim verebiliriz.
+FOREIGN KEY (ClassID) REFERENCES [dbo].[Class](ClassID)  --Ýki table daki classýd sayýlarý farklý olursa hata veriyor.
+
+
+ALTER TABLE [dbo].[Course]
+--DROP FOREIGN KEY FKClassCourse
+ADD CONSTRAINT FKClassCourse      
+FOREIGN KEY (ClassID) REFERENCES [dbo].[Class](ClassID)
+
+
+ALTER TABLE [dbo].[Course]
+ADD CONSTRAINT FKCourseTutor     
+FOREIGN KEY (TutorID) REFERENCES [dbo].[Tutor](TutorID)
+
+ALTER TABLE [dbo].[Student]
+ADD CONSTRAINT FKClassStudent   
+FOREIGN KEY (ClassID) REFERENCES [dbo].[Class](ClassID)
+
+
+select TutorID from Tutor    --HATA VERÝNCE NEREDEN KAYNAKLANDIÐINI BULMAK ÝÇÝN KULLANDIK.
+WHERE TutorID IN
+(SELECT TutorID from Course)
+
+ALTER TABLE [dbo].[Enrollment]
+ADD CONSTRAINT FKStudentClassEnroll   
+FOREIGN KEY (StudentID) REFERENCES [dbo].[Student](StudentID) 
+
+ALTER TABLE [dbo].[Enrollment]
+ADD CONSTRAINT FKStudentClassEnroll   
+FOREIGN KEY (StudentID) REFERENCES [dbo].[Student](StudentID) 
+
+ALTER TABLE [dbo].[Success]
+ADD CONSTRAINT FKStudentSuccess  
+FOREIGN KEY (StudentID) REFERENCES [dbo].[Student](StudentID) 
+
+ALTER TABLE [dbo].[Enrollment]
+ADD CONSTRAINT FKCourseEnroll   
+FOREIGN KEY (CourseID) REFERENCES [dbo].[Course](CourseID)
+
+ALTER TABLE [dbo].[Success]
+ADD CONSTRAINT FKCourseSuccess  
+FOREIGN KEY (CourseID) REFERENCES [dbo].[Course](CourseID)
+
