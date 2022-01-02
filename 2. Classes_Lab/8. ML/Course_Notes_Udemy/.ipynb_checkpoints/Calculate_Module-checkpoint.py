1e9d78902c8ac017a9b0c417b7bb1cd1980aedda
StephenKinggg <87016594+StephenKinggg@users.noreply.github.com>
2022-01-03 00:01:05 +0300

    some files added and updated.

---
+++ 2. Classes_Lab/8. ML/Course_Notes_Udemy/.ipynb_checkpoints/Calculate_Module-checkpoint.py
@@ -0,0 +1,36 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "39a9e6eb-f7e7-4c09-9941-b7fe128fddf7",
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "def yeni_maas(x):\n",
+    "    print(x*20/100 + x)"
+   ]
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "Python 3",
+   "language": "python",
+   "name": "python3"
+  },
+  "language_info": {
+   "codemirror_mode": {
+    "name": "ipython",
+    "version": 3
+   },
+   "file_extension": ".py",
+   "mimetype": "text/x-python",
+   "name": "python",
+   "nbconvert_exporter": "python",
+   "pygments_lexer": "ipython3",
+   "version": "3.9.7"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 5
+}
