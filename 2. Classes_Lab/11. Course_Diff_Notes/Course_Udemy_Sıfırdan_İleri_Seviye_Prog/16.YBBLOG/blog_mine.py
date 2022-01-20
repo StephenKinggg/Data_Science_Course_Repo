f63e55026c081e43e75817e1fdcbc8a15292a4ff
StephenKinggg <87016594+StephenKinggg@users.noreply.github.com>
2022-01-20 15:52:29 +0300

    some files added.

--- 2. Classes_Lab/11. Course_Diff_Notes/Course_Udemy_Sıfırdan_İleri_Seviye_Prog/16.YBBLOG/blog_mine.py
+++ 2. Classes_Lab/11. Course_Diff_Notes/Course_Udemy_Sıfırdan_İleri_Seviye_Prog/16.YBBLOG/blog_mine.py
@@ -1,6 +1,18 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)  # Web sunucumuzu kendi bs çalıştıracağımızdan dolayı bir obj. oluşturduk.
+
+@app.route("/")  # ben bir adrese gitmek istiyorum. içine de adresi yazıyoruz. request roote olarak yazdık.
+def index(): # request yazdık.
+    return " Ana sayfa"  # böyle bir yazı dönsün yani bu response olacak. Tekrar localhost çalıştırınca işlem sonucunu görebiliriz.
+
+@app.route("/about")   #bu işlem sonucu localhost:5000/about yazınca return işlemi sonucunu göreceğiz.
+def about():
+    return "Hakkımda"
+
+@app.route("/about/kisi") 
+def kisi():
+    return " This page is about person."
 
 if __name__ =="__main__" : # bir web sunucusunu çalıştırıyoruz.bir tane localhost çalıştırıyoruz.
     app.run(debug=True) # içine bir tane parametre verdik. hatalarımızı web sitemizde görelim. çünkü geliştirme aşamasındayız.
@@ -18,7 +30,10 @@ if __name__ =="__main__" : # bir web sunucusunu çalıştırıyoruz.bir tane loc
 # Çalışırken bunların her zaman açık olması lazım.
 # Daha sonra aynı yerden mysql admin açıyoruz. Bu açılan ekrandan herhangi bir sql sorgusunu çalıştırabiliyoruz.add()
 # Tekrar local host açtık. Burada her bir request için bir response alacağız. 
-# Yani her bir url adresi talebine karşın flask ta bir func bulunur.
+# Yani her bir url adresi talebine karşın flask ta bir func bulunur. Bunun için hazır bir decorator mantığı kullanmamız gerekir.
 
+# Yukarıda önce string template döndürmüştük şimdi ise bir html yani jinja template döndürmek istiyorum.
+# bunu render ederek göndermek zorundayız. Bu durumda içine pyhton kodu yazabiliyorum.
+# render_template func indirdik.
 
 
