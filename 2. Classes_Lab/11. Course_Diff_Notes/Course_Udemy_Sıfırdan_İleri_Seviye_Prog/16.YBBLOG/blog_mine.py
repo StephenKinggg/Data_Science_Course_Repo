60eb7ea6d8f772a51eb1d4dd33e497886a9a32a9
StephenKinggg <87016594+StephenKinggg@users.noreply.github.com>
2022-01-21 11:08:30 +0300

    some files updated.

--- 2. Classes_Lab/11. Course_Diff_Notes/Course_Udemy_Sıfırdan_İleri_Seviye_Prog/16.YBBLOG/blog_mine.py
+++ 2. Classes_Lab/11. Course_Diff_Notes/Course_Udemy_Sıfırdan_İleri_Seviye_Prog/16.YBBLOG/blog_mine.py
@@ -4,7 +4,11 @@ app = Flask(__name__)
 
 @app.route("/")  
 def index():   
-    return render_template("index.html")  #default olarak templates klasörüne gitti.
+    return render_template("index.html", islem=1)  # önce layout.html yaptım sonra bundan inheritance yapmak için inde.html yazdık.
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
 
 if __name__ =="__main__" : 
     app.run(debug=True)
