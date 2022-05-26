c2e79394ac99cc583b10c83474a29e9e0340b49c
StephenKinggg <87016594+StephenKinggg@users.noreply.github.com>
2022-05-26 15:14:57 +0300

    some files updated.

--- 2. Classes_Lab/4. DAwSQL/SQLQuery2_IN(FOUTER-CROSS-SELF)_16.10.21.sql
+++ 2. Classes_Lab/4. DAwSQL/SQLQuery2_IN(FOUTER-CROSS-SELF)_16.10.21.sql
@@ -5,20 +5,65 @@
 -- Maðaza çalýþanlarýný yaptýklarý satýþlar ile birlikte listeleyin
 
 
-SELECT S.staff_id, S.first_name, S.last_name, O.*
+SELECT *
 FROM sale.staff S
-INNER JOIN sale.orders O ON S.staff_id=O.staff_id
-ORDER BY O.order_id DESC
 
-SELECT A.staff_id, A.first_name, A.last_name, B.*
+SELECT *
+FROM sale.orders O
+
+--Aþaðýdaki query ile maðaza çalýþanlarýndan sadece sipariþ alanlarý getirdim. Çünkü Inner Join kullandým. 
+--Sipariþ almayan maðaza çalýþanlarýný getirmedi. Bundan dolayý InnerJoin iþime yaramýyor.
+
+SELECT A.staff_id, A.first_name, A.last_name, B.staff_id, B.order_id --id ler ile çalýþmak her zaman çalýþmamýzýn doðruluðunu artýrýr.
+FROM sale.staff A
+INNER JOIN sale.orders B ON A.staff_id=B.staff_id
+ORDER BY A.staff_id -- Default ASC
+
+-- Maðaza çalýþanlarýndan hangileri satýþ yapýyor ona baktýk.
+
+SELECT A.staff_id, A.first_name, A.last_name, B.staff_id, B.order_id 
 FROM sale.staff A
 LEFT JOIN sale.orders B ON A.staff_id=B.staff_id
-ORDER BY B.order_id 
+ORDER BY A.staff_id
 
-SELECT COUNT(A.staff_id), COUNT(B.staff_id)
+
+SELECT COUNT(A.staff_id), COUNT(B.staff_id) -- Satýr sayýsýný getirdi.
 FROM sale.staff A
 INNER JOIN sale.orders B ON A.staff_id=B.staff_id
 
-SELECT COUNT(DISTINCT A.staff_id), COUNT(DISTINCT B.staff_id)
+
+--INNER JOIN VE LEFT JOIN ile bakýnca aradaki farka dikkat edelim.
+--INNER JOIN de ikiside ayný iken LEFT JOIN de staff tablosu tamamý gelirken yani 10, orders sipariþ alanlar geliyor.
+
+SELECT COUNT(DISTINCT A.staff_id), COUNT(DISTINCT B.staff_id) --
+FROM sale.staff A
+INNER JOIN sale.orders B ON A.staff_id=B.staff_id
+
+
+SELECT COUNT(DISTINCT A.staff_id), COUNT(DISTINCT B.staff_id) --
 FROM sale.staff A
 LEFT JOIN sale.orders B ON A.staff_id=B.staff_id
+
+------ CROSS JOIN ------
+
+-- Hangi markada hangi kategoride kaçar ürün olduðu bilgisine ihtiyaç duyuluyor
+
+SELECT DISTINCT A.brand_id, A.brand_name, C.category_id, C.category_name
+FROM product.brand A
+LEFT JOIN product.product B ON A.brand_id=B.brand_id
+CROSS JOIN product.category C
+ORDER BY A.brand_id
+
+------ SELF JOIN ------
+
+-- Personelleri ve þeflerini listeleyin
+-- Çalýþan adý ve yönetici adý bilgilerini getirin
+
+SELECT *
+FROM sale.staff
+
+SELECT B.first_name, A.first_name AS manager_name
+FROM sale.staff A
+INNER JOIN sale.staff B ON A.staff_id=B.manager_id
+
+
--- 2. Classes_Lab/4. DAwSQL/SQLQuery3_IN(HAVINGS-GRPSETS-ROLLUP-CUBE_PİVOT)_16.10.21.sql
+++ 2. Classes_Lab/4. DAwSQL/SQLQuery3_IN(HAVINGS-GRPSETS-ROLLUP-CUBE_PİVOT)_16.10.21.sql
@@ -1,33 +1,21 @@
------- CROSS JOIN ------
-
--- Hangi markada hangi kategoride kaçar ürün olduðu bilgisine ihtiyaç duyuluyor
-
-
-
------- SELF JOIN ------
-
--- Personelleri ve þeflerini listeleyin
--- Çalýþan adý ve yönetici adý bilgilerini getirin
-
-
 
 --------Advanced Grouping Operations
 
+----Write a query that checks if any product id is repeated in more than one row in the products table.
 ----product tablosunda herhangi bir product id' nin çoklayýp çoklamadýðýný kontrol ediniz.
 
+SELECT *
+FROM product.product
 
 SELECT product_id, COUNT(*) CNT_ROW
 FROM product.product 
 GROUP BY product_id
-HAVING COUNT(*) > 1
+HAVING COUNT(*) > 1 --Agg.sonucu ortaya çýkan yeni sütunda bir filtreleme yapýyorum.
 
-
+--Write a query that returns category ids with a max list price above 4000 or a min list price below 500.
 --maximum list price' ý 4000' in üzerinde olan veya minimum list price' ý 500' ün altýnda olan categori id' leri getiriniz
 --category name' e gerek yok.
 
---SELECT	category_id, list_price
---FROM	product.product
---ORDER BY 1,2
 
 SELECT category_id, MAX(list_price) MAX_PRICE, MIN(list_price) MIN_PRICE
 FROM product.product
@@ -47,31 +35,65 @@ FROM product.product
 GROUP BY category_id
 HAVING MAX(list_price) > 4000 AND MIN(list_price) < 500
 
-
+--Find the average product prices of the brands.
 --Markalara ait ortalama ürün fiyatlarýný bulunuz.
 --ortalama fiyatlara göre azalan sýrayla gösteriniz.
 
-SELECT A.brand_id, A.brand_name, AVG(B.list_price) AVG_PRICE
-FROM product.brand A
-INNER JOIN product.product B ON A.brand_id=B.brand_id
-GROUP BY A.brand_id, A.brand_name
-ORDER BY AVG(B.list_price) DESC
+SELECT B.brand_name, AVG(A.list_price) AVG_PRICE
+FROM product.product A
+INNER JOIN product.brand B ON A.brand_id=B.brand_id
+GROUP BY B.brand_name
+ORDER BY AVG(A.list_price) DESC
 
+///***
+SELECT	B.brand_id, B.brand_name, AVG(A.list_price) avg_price
+FROM	product.product A, product.brand B
+WHERE	A.brand_id = B.brand_id
+GROUP BY
+		B.brand_id, B.brand_name
+ORDER BY 
+		avg_price DESC
+***///
 
+---Write a query that returns BRANDS with an average product price of more than 1000.
 ---ortalama ürün fiyatý 1000' den yüksek olan MARKALARI getiriniz
 
-SELECT A.brand_name, AVG(B.list_price) AVG_PRICE
-FROM product.brand A
-INNER JOIN product.product B ON A.brand_id=B.brand_id
-GROUP BY A.brand_name
-HAVING AVG(B.list_price) > 1000
-ORDER BY AVG(B.list_price) ASC
+SELECT B.brand_name, AVG(A.list_price) AVG_PRICE
+FROM product.product A
+INNER JOIN product.brand B ON A.brand_id=B.brand_id
+GROUP BY B.brand_name
+HAVING AVG(A.list_price) > 1000
+ORDER BY AVG(A.list_price) ASC  --AVG_PRICE da kullanabilirdik.
 
+
+--Write a query that returns the net price by the customer for each order.(Dont neglect discounts and quantities)
 --bir sipariþin toplam net tutarýný getiriniz. (müþterinin sipariþ için ödediði tutar)
 --discount' ý ve quantity' yi ihmal etmeyiniz.
 
 
+SELECT *
+FROM sale.order_item
+
+SELECT *
+FROM sale.orders
+
+--Herbir müþteri tarafýndan verilen sipariþ toplamlarý
+SELECT A.customer_id, A.order_id, SUM ((B.list_price*(1-B.discount))*B.quantity)
+FROM sale.orders A
+INNER JOIN sale.order_item B ON A.order_id=B.order_id
+GROUP BY A.customer_id, A.order_id
+ORDER BY A.customer_id
+
+--Verilen her bir sipariþin toplam tutarý
+SELECT A.order_id, SUM ((B.list_price*(1-B.discount))*B.quantity)
+FROM sale.orders A
+INNER JOIN sale.order_item B ON A.order_id=B.order_id
+GROUP BY A.order_id
+ORDER BY A.order_id
+
+
 --SELECT ... INTO FROM ...
+--Aþaðýda 4 tabloyu kullanarak aþaðýdaki columnleri içeren brand name, category name, model year göre bir gruplama yaparak yeni özet bir tablo oluþturuyoruz.
 
 SELECT	C.brand_name as Brand, D.category_name as Category, B.model_year as Model_Year,
 		ROUND (SUM (A.quantity * A.list_price * (1 - A.discount)), 0) total_sales_price
@@ -120,12 +142,20 @@ SELECT brand, category, SUM(total_sales_price) total_sales
 FROM sale.sales_summary
 GROUP BY 
 	GROUPING SETS(
-	         (brand, category),
-			 (brand),
-			 (category),
-			 ()
+	         (brand, category), -- yukarýdaki brand ve category göre gruplandýrma
+			 (brand),           -- yukarýdaki brand göre gruplandýrma
+			 (category),        -- yukarýdaki category göre gruplandýrma
+			 ()                 -- yukarýdaki gruplandýrma yapmadan toplam sales verir.
 			 )
 ORDER BY brand, category
 
--------brand, category, model_year sütunlarý için Rollup kullanarak total sales hesaplamasý yapýnýz.
+--Yukarýdaki query göre çýkan tabloda () olunca sadece total_sales yazacak,category veya branda göre bu satýrlar dolu olacak, category ve brand göre iki sütun dolu olacak.
+
+
+--brand, category, model_year sütunlarý için Rollup kullanarak total sales hesaplamasý yapýnýz.
 --üç sütun için 4 farklý gruplama varyasyonu incelemeye çalýþýnýz.
+
+SELECT brand, category,model_year, SUM(total_sales_price) total_sales
+FROM sale.sales_summary
+GROUP BY 
+	ROLLUP (brand, category, model_year) 
